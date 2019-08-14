18cd29a5a20601606995a4625768c4e1ceafbdf7
Regan Wolfrom <regan.wolfrom@gmail.com>
2019-08-13 23:23:50 -0500

    moved results to explorer component from searchbar adjusted spinner added userId and contentType to results

--- src/app/plugins/document-explorer/pages/explore/explore.component.ts
+++ src/app/plugins/document-explorer/pages/explore/explore.component.ts
@@ -1,25 +1,49 @@
-import {
-    Component,
-    OnInit
-  } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Document } from '../../../../services/api.schema';
+import { SearchbarComponent } from '../../components/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-docex-explore',
   templateUrl: './explore.component.html',
   styleUrls: ['./explore.component.scss']
 })
-export class ExploreComponent implements OnInit {
+export class ExploreComponent implements OnInit, AfterViewInit {
 
+  results$: Observable<{} | Document[]>;
+  dateSearchSubmitted = false;
+  tagSearchSubmitted = false;
+  @ViewChild('searchbar', {static: false}) searchbar: SearchbarComponent;
 
   constructor(private router: Router) {}
 
   ngOnInit() {
+  }
 
+  ngAfterViewInit() {
+    this.searchbar.dateFormSubmitted$.subscribe(
+      (submitted) => {
+        this.dateSearchSubmitted = submitted;
+        this.results$ = null;
+      });
+    this.searchbar.tagFormSubmitted$.subscribe(
+      (submitted) => {
+        this.tagSearchSubmitted = submitted;
+        this.results$ = null;
+      });
   }
 
   addDocuments() {
     this.router.navigate(['/documents/add']);
+  }
+
+  displayResults(results) {
+    this.dateSearchSubmitted = false;
+    this.tagSearchSubmitted = false;
+    this.results$ = Observable.create((observer) => {
+      observer.next(results);
+    });
   }
 
 }
